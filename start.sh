2479933637b5a0e09501003692181937095e23cb
sergio <sergio@ksergio.com>
2024-01-15 14:16:46 +0000

    Fix apend

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ ACCESS_TOKEN=$(printenv ACCESS_TOKEN)
 ACCESS_TOKEN_SECRET=$(printenv ACCESS_TOKEN_SECRET)
 
 echo "MONGO_URI=$MONGO_URI" > /app/cron/cronjob
-echo "CONSUMER_KEY=$CONSUMER_KEY" > /app/cron/cronjob
-echo "CONSUMER_SECRET=$CONSUMER_SECRET" > /app/cron/cronjob
-echo "ACCESS_TOKEN=$ACCESS_TOKEN" > /app/cron/cronjob
-echo "ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET" > /app/cron/cronjob
+echo "CONSUMER_KEY=$CONSUMER_KEY" >> /app/cron/cronjob
+echo "CONSUMER_SECRET=$CONSUMER_SECRET" >> /app/cron/cronjob
+echo "ACCESS_TOKEN=$ACCESS_TOKEN" >> /app/cron/cronjob
+echo "ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET" >> /app/cron/cronjob
 
 echo "0 * * * * /usr/local/bin/python3 /app/script/main.py >> /app/cron/cron.log 2>&1" >> /app/cron/cronjob
 
